01b60e2b1317cdaa9de0d61035a087602f62c999
Santiago Cabrera <scabrera@foqum.io>
2023-03-27 15:56:00 +0200

    added the function api-call-delete.js and the button to delete a tournament now calls this function

--- client/src/Tournament.svelte
+++ client/src/Tournament.svelte
@@ -1,6 +1,7 @@
 <script>
   import { onMount } from "svelte";
   import apiCall from "utils/api-call.js";
+  import apiCallDelete from "utils/api-call-delete.js";
 
   export let params;
 
@@ -17,9 +18,15 @@
   });
 
   let deleting = false;
+  let error = null;
 
   async function handleDelete() {
     deleting = true;
+    try {
+      const { data, isSuccess } = await apiCallDelete("tournament/" + tournament.code);
+    } catch (e) {
+      console.error(e);
+    }
     deleting = false;
   }
 </script>
@@ -29,6 +36,6 @@
 {#if tournament}
   <p>Name: {tournament.name}</p>
   <div class="form-control w-full max-w-xs">
-    <button class="btn" on:click={handleCreate} disabled={deleting}>Delete Tournament</button>
+    <button class="btn" on:click={handleDelete} disabled={deleting}>Delete Tournament</button>
   </div>
 {/if}
